9bb5d5d36b3013d5e4f5ca1319b1cf507d1acbd6
lucas amorim de lima araújo <lucas.ala1999@gmail.com>
2019-02-25 10:01:13 -0300

    cloud function done

--- cloud/main.js
+++ cloud/main.js
@@ -1,40 +1,22 @@
+"use strict";
+
 Parse.Cloud.define('cep', (request, response) => {
-    const { Cep } = request.params;
-    const cepRegex = RegExp('^\\d{5}\-?\\d{3}$');//regex for Cep
+    const cep = request.params['cep'];
+    const cepRegex = RegExp('^\\d{5}\-?\\d{3}$');
 
-    if(Cep.match(cepRegex)){
-        //call the api
+    if (cep.match(cepRegex)) {
         Parse.Cloud.httpRequest({
-        url: `http://viacep.com.br/ws/${ Cep }/json/`  
-      })
-      .then(function(httpResponse) {
-        //success call back
-        let myJSON = httpResponse.text;
-
-        if(myJSON == "{\n  \"erro\": true\n}"){
-          //Error to verficate the existence of the CEP
-          console.log(httpResponse.text);
-          console.log("This CEP dont exist :(");
-          myJSON = {"valid": false};
-          console.log(myJSON);
-        }
-
-        else{
-          //Success to find the CEP
-          console.log(httpResponse.text);
-        }
-      },  
-      function(httpResponse) {
-        //Error call back
-        'response code ' + httpResponse.status;
-      });
+            url: `http://viacep.com.br/ws/${cep}/json/`  
+        }).then((httpResponse) => {
+            if (httpResponse.text === "{\n  \"erro\": true\n}") {
+                response.success({'valid': false});
+            } else {
+                response.success(httpResponse.data);
+            }
+        }, (httpResponse) => {
+            response.error(httpResponse.status);
+        });
+    } else {
+        response.error({'valid': false});
     }
-
-      else{
-        //Error to validate the CEP
-        let myJSON = {"valid": false}; 
-        console.log(myJSON);
-        console.log('this Cep is not valid :(')
-      }   
-    
 });
